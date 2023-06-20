678f75b9b9a4217ea7e5f66f43e6903ee4973f43
Hamit <sadecehamit@gmail.com>
2023-06-20 21:41:26 +0200

    20.06.2023

---
+++ src/test/java/techproed/day16_SeleniumWait/C03_FluentWait.java
@@ -0,0 +1,13 @@
+package techproed.day16_SeleniumWait;
+
+import org.junit.Test;
+import techproed.utilities.TestBase;
+
+public class C03_FluentWait extends TestBase {
+    @Test
+    public void test01() {
+
+
+
+    }
+}
